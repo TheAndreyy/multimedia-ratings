9c62a42671567bd913522917c3c329f1b87b2037
TheAndreyy <andrzej.kusiak00@gmail.com>
2022-02-28 00:38:37 +0100

    refactor(#3): Setting up Kafka producer

---
+++ ratings-api/src/main/kotlin/com/example/ratingsapi/config/KafkaTopicConfig.kt
@@ -0,0 +1,13 @@
+package com.example.ratingsapi.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.kafka.config.TopicBuilder
+
+@Configuration
+class KafkaTopicConfig {
+
+    @Bean
+    fun testTopic() = TopicBuilder.name("Test").build()
+
+}
